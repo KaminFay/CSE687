d998155c6fe5b0324b123e2f9e0e7e60d1ab6947
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-05-09 22:08:54 -0700

    Temp: display objects in list view with toggle switch... need to correct layout

---
+++ guiApp/DLLObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace guiApp
+{
+    internal class DLLObject : INotifyPropertyChanged
+    {
+        private string name;
+        private string v;
+
+        public DLLObject(string v)
+        {
+            this.v = v;
+        }
+
+        private string Name
+        {
+            get => name;
+            set
+            {
+                if (value == name) return;
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void OnPropertyChanged()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- guiApp/MainPage.xaml.cs
+++ guiApp/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -64,6 +65,9 @@ namespace guiApp
 
         private async void OpenFilePicker_Click(object sender, RoutedEventArgs routedEventArgs)
         {
+            // Clear any previously returned files between iterations of this scenario
+            //OutputTextBlock.Text = "";
+
             FileOpenPicker openPicker = new FileOpenPicker();
             openPicker.ViewMode = PickerViewMode.List;
             openPicker.SuggestedStartLocation = PickerLocationId.Desktop;
@@ -71,13 +75,35 @@ namespace guiApp
             IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();
             if (files.Count > 0)
             {
-                StringBuilder output = new StringBuilder();
+                StringBuilder output = new StringBuilder("Selected DLLs: \n");
 
                 foreach (StorageFile file in files)
                 {
                     output.Append(file.Name + "\n");
                 }
+                //OutputTextBlock.Text = output.ToString();
             }
+            else
+            {
+                //OutputTextBlock.Text = "Operation cancelled.";
+            }
+
+        }
+
+        private void ListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var listItems = new ObservableCollection<DLLObject>();
+
+            for (int i = 0; i < 5; i ++)
+            {
+                listItems.Add(new DLLObject($"item {i}"));
+            }
+
+            this.Items.ItemsSource = listItems;
+        }
+
+        private void Toggle_Toggled(object sender, RoutedEventArgs e)
+        {
 
         }
     }
