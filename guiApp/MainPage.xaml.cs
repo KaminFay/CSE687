d0c49058ede828a43c8b14c138e60d3d17b9e345
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-05-25 19:18:34 -0700

    Implementation of SocketManager to connect to tes server... eventually test harness

--- guiApp/MainPage.xaml.cs
+++ guiApp/MainPage.xaml.cs
@@ -67,64 +67,9 @@ namespace guiApp
             //    Console.WriteLine(ex.ToString());
             //}
 
-            byte[] bytes = new byte[1024];
 
-            try
-            {
-                IPHostEntry host = Dns.GetHostEntry("localhost");
-                IPAddress ipAddress = host.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
-
-                // Create a TCP/IP  socket.    
-                Socket sender = new Socket(ipAddress.AddressFamily,
-                    SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect the socket to the remote endpoint. Catch any errors.    
-                try
-                {
-                    // Connect to Remote EndPoint  
-                    sender.Connect(remoteEP);
-
-                    Console.WriteLine("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString());
-
-                    // Encode the data string into a byte array.    
-                    byte[] msg = Encoding.ASCII.GetBytes("Hello World! <EOF>");
-
-                    // Send the data through the socket.    
-                    int bytesSent = sender.Send(msg);
-
-                    // Receive the response from the remote device.    
-                    int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-                    // Release the socket.    
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-                }
-                catch (ArgumentNullException ane)
-                {
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-
-
+            SocketManager.connectToServerSocket();
+           
         }
 
         private void OnElementClicked(Object sender, RoutedEventArgs routedEventArgs)
@@ -213,6 +158,16 @@ namespace guiApp
                 //_ = enumerator.Current;
 
             }
+        }
+
+        private void RichEditBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Run_Test_Execution(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
